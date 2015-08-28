5296d56170fee128395041cfdbe8b4dda63bc64b
NieYong <aofengblog@163.com>
2015-08-28 11:10:57 +0800

    1、重构：只提供加密和解密操作，具体的灱操作由具体的子类实现。 2、完善注释。

--- src/cn/aofeng/demo/encrypt/EncryptAndDecrypt.java
+++ src/cn/aofeng/demo/encrypt/EncryptAndDecrypt.java
@@ -13,10 +13,6 @@ import javax.crypto.SecretKey;
 import javax.crypto.spec.IvParameterSpec;
 import javax.crypto.spec.SecretKeySpec;
 
-import org.apache.commons.codec.binary.Base64;
-
-import static cn.aofeng.demo.util.LogUtil.log;;
-
 /**
  * 加密与解密。
  * 
@@ -25,123 +21,96 @@ import static cn.aofeng.demo.util.LogUtil.log;;
 public class EncryptAndDecrypt {
 
     private final static String CHARSET = "utf8";
-    
-    private SecretKey createSecretKey(String encryptType, String keyStr)
+
+    /**
+     * 创建安全密钥。
+     * 
+     * @param encryptType
+     *            加密方式，如：AES，Blowfish。详情查看<a href="https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher">Java Cryptography Architecture Standard Algorithm Name Documentation</a>
+     * @param keyStr
+     *            密钥明文
+     * @return 安全密钥
+     * @throws UnsupportedEncodingException
+     *             不支持指定的字符集编码
+     */
+    public SecretKey createSecretKey(String encryptType, String keyStr)
             throws UnsupportedEncodingException {
         byte[] secretKeyData = keyStr.getBytes(CHARSET);
         SecretKeySpec sks = new SecretKeySpec(secretKeyData, encryptType);
-        
+
         return sks;
     }
-    
-    private byte[] encrypt(String encryptType, SecretKey secretKey, String srcData)
-            throws InvalidKeyException, IllegalBlockSizeException,
-            BadPaddingException, UnsupportedEncodingException,
-            NoSuchAlgorithmException, NoSuchPaddingException, 
-            InvalidAlgorithmParameterException {
+
+    /**
+     * 加密数据。
+     * 
+     * @param encryptType 加密方式，如：AES，Blowfish。详情查看<a href="https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher">Java Cryptography Architecture Standard Algorithm Name Documentation</a>
+     * @param secretKey 密钥
+     * @param srcData 待加密的数据
+     * @return 数据加密后的字节数组
+     * @see #encrypt(String, SecretKey, String, String)
+     */
+    public byte[] encrypt(String encryptType, SecretKey secretKey,
+            String srcData) throws InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException,
+            UnsupportedEncodingException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException {
         return encrypt(encryptType, secretKey, srcData, null);
     }
-    
-    private byte[] encrypt(String encryptType, SecretKey secretKey, String srcData, String algorithmParam)
-            throws InvalidKeyException, IllegalBlockSizeException,
-            BadPaddingException, UnsupportedEncodingException,
-            NoSuchAlgorithmException, NoSuchPaddingException, 
-            InvalidAlgorithmParameterException {
+
+    public byte[] encrypt(String encryptType, SecretKey secretKey,
+            String srcData, String algorithmParam) throws InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException,
+            UnsupportedEncodingException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException {
         Cipher encrpyt = Cipher.getInstance(encryptType);
         if (null == algorithmParam) {
             encrpyt.init(Cipher.ENCRYPT_MODE, secretKey);
         } else {
-            IvParameterSpec iv = new IvParameterSpec(algorithmParam.getBytes(CHARSET));
+            IvParameterSpec iv = new IvParameterSpec(
+                    algorithmParam.getBytes(CHARSET));
             encrpyt.init(Cipher.ENCRYPT_MODE, secretKey, iv);
         }
         byte[] secretData = encrpyt.doFinal(srcData.getBytes(CHARSET));
-        
+
         return secretData;
     }
-    
-    private String decrypt(String decryptType, SecretKey secretKey, byte[] secretData)
+
+    /**
+     * 解密数据。
+     * 
+     * @param decryptType 解密方式，如：AES，Blowfish。详情查看<a href="https://docs.oracle.com/javase/7/docs/technotes/guides/security/StandardNames.html#Cipher">Java Cryptography Architecture Standard Algorithm Name Documentation</a>
+     * @param secretKey 密钥
+     * @param secretData 待解密的数据
+     * @return 解密后的数据
+     * @see #decrypt(String, SecretKey, byte[], String)
+     */
+    public String decrypt(String decryptType, SecretKey secretKey,
+            byte[] secretData) throws InvalidKeyException,
+            IllegalBlockSizeException, BadPaddingException,
+            UnsupportedEncodingException, NoSuchAlgorithmException,
+            NoSuchPaddingException, InvalidAlgorithmParameterException {
+        return decrypt(decryptType, secretKey, secretData, null);
+    }
+
+    public String decrypt(String decryptType, SecretKey secretKey,
+            byte[] secretData, String algorithmParam)
             throws InvalidKeyException, IllegalBlockSizeException,
             BadPaddingException, UnsupportedEncodingException,
             NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidAlgorithmParameterException {
-        return decrypt(decryptType, secretKey, secretData, null);
-    }
-    
-    private String decrypt(String decryptType, SecretKey secretKey, byte[] secretData, String algorithmParam)
-            throws InvalidKeyException, IllegalBlockSizeException,
-            BadPaddingException, UnsupportedEncodingException,
-            NoSuchAlgorithmException, NoSuchPaddingException, 
             InvalidAlgorithmParameterException {
         Cipher decrypt = Cipher.getInstance(decryptType);
         if (null == algorithmParam) {
             decrypt.init(Cipher.DECRYPT_MODE, secretKey);
         } else {
-            IvParameterSpec iv = new IvParameterSpec(algorithmParam.getBytes(CHARSET));
+            IvParameterSpec iv = new IvParameterSpec(
+                    algorithmParam.getBytes(CHARSET));
             decrypt.init(Cipher.DECRYPT_MODE, secretKey, iv);
         }
-        byte[] srcData =  decrypt.doFinal(secretData);
+        byte[] srcData = decrypt.doFinal(secretData);
         String srcStr = new String(srcData, CHARSET);
-        
+
         return srcStr;
-    }
-    
-    private void blowfishPerformence(String data)
-            throws UnsupportedEncodingException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException,
-            NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidAlgorithmParameterException {
-        SecretKey secretKey;
-        String type = "Blowfish";
-        secretKey = createSecretKey(type, "abcdefgh_1234567");
-        long startTime = System.currentTimeMillis();
-        for (int j = 0; j < 100000; j++) {
-            encrypt(type, secretKey, data+j);
-        }
-        long endTime = System.currentTimeMillis();
-        long usedTime = endTime - startTime;
-        log("使用%s进行%d次加密消耗时间%d毫秒", type, 100000, usedTime);
-    }
-    
-    private void aesPerformence(String data)
-            throws UnsupportedEncodingException, InvalidKeyException,
-            IllegalBlockSizeException, BadPaddingException,
-            NoSuchAlgorithmException, NoSuchPaddingException,
-            InvalidAlgorithmParameterException {
-        SecretKey secretKey;
-        String type = "AES/CBC/PKCS5Padding";
-        secretKey = createSecretKey("AES", "abcdefgh_1234567");
-        long startTime = System.currentTimeMillis();
-        for (int j = 0; j < 100000; j++) {
-            encrypt(type, secretKey, data+j, "abcdefgh12345678");
-        }
-        long endTime = System.currentTimeMillis();
-        long usedTime = endTime - startTime;
-        log("使用%s进行%d次加密消耗时间%d毫秒", type, 100000, usedTime);
-    }
-    
-    public static void main(String[] args) throws NoSuchAlgorithmException, 
-            NoSuchPaddingException, UnsupportedEncodingException,
-            InvalidKeyException, IllegalBlockSizeException, 
-            BadPaddingException, InvalidAlgorithmParameterException {
-        String data = "炎黄，汉字，english,do it,abcdefghijklmnopqrstuvwxyz,0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZ, ~!@#$%^&*()_+=-";
-        log("待加密的数据：\n%s", data);
-        
-        String encryptType = "Blowfish";
-        EncryptAndDecrypt bf = new EncryptAndDecrypt();
-        SecretKey secretKey = bf.createSecretKey(encryptType, "abcdefgh_1234567");
-        byte[] secretData = bf.encrypt(encryptType, secretKey, data);
-        log("使用%s加密后的数据：", encryptType);
-        log(Base64.encodeBase64String(secretData));
-        
-        String srcStr = bf.decrypt(encryptType, secretKey, secretData);
-        log("解密后的数据：\n%s", srcStr);
-        
-        // ==========性能测试==========
-        // AES
-        bf.aesPerformence(data);
-        
-        // Blowfish
-        bf.blowfishPerformence(data);
     }
 
 }
